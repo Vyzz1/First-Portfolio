6efa2ee29d2406b1128df95556324b0b68b7b7a8
Huỳnh Khang Vỹ <52200024@student.tdtu.edu.vn>
2025-05-24 20:35:58 +0700

    refactor: Enhance metadata and openGraph details in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,28 @@ import "./globals.css";
 import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "Huỳnh Khang Vỹ Portfolio",
-  description: "Modern and minimalist design",
+  description:
+    "Explore Huỳnh Khang Vỹ's portfolio, a passionate student showcasing modern, minimalist, and inspiring design projects.",
+  openGraph: {
+    title: "Huỳnh Khang Vỹ Portfolio",
+    description:
+      "Explore Huỳnh Khang Vỹ's portfolio, a passionate student showcasing modern, minimalist, and inspiring design projects.",
+    url: "https://huynh-khang-vy.vercel.app",
+    siteName: "Huỳnh Khang Vỹ Portfolio",
+    locale: "vi_VN",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dl8h3byxa/image/upload/v1748093680/473346275_4222714344616696_8111006687165331402_n.jpg_pksfog.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Huỳnh Khang Vỹ Portfolio",
+      },
+    ],
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
