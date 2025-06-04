7c6d9007592a5d2f2c18d474c24df428c69af552
Huỳnh Khang Vỹ <52200024@student.tdtu.edu.vn>
2025-06-04 16:05:03 +0700

    refactor: Add robots meta tag for SEO optimization in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,17 @@ export const metadata: Metadata = {
       },
     ],
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 export default function RootLayout({
   children,
